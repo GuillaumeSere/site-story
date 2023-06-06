63ac2ae88d0905d67fdd773e38794e8480fe28fe
Guillaume Sere <guillaumesere@hotmail.fr>
2023-06-06 10:48:47 +0200

    add pagination

--- src/components/sites/Sites.jsx
+++ src/components/sites/Sites.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
-import './sites.css'
-import { BsArrowRight, BsGithub } from 'react-icons/bs'
-import data from '../data/Data'
+import React, { useState } from 'react';
+import './sites.css';
+import { BsArrowRight, BsGithub } from 'react-icons/bs';
+import data from '../data/Data';
 
 const Sites = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const cardsPerPage = 6;
+
+    // Calculer l'index de début et de fin des cartes à afficher sur la page actuelle
+    const indexOfLastCard = currentPage * cardsPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    const currentCards = data.slice(indexOfFirstCard, indexOfLastCard);
+
+    // Fonction pour changer de page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
+        <>
         <div className='container'>
-            {
-                data.map(({ id, image, title, demo, git, qr }) => {
-                    return (
-                        <div className="card" key={id}>
-                            <div className="card-image">
-                                <img src={image} alt="ilustartion site" />
-                                <a href={demo} target="_blank" rel="noreferrer" className='card-link'>{title}</a>
-                                <a href={git} target="_blank" rel="noreferrer"><BsGithub className='link' /></a>
-                            </div>
-                            <div className="card-qr">
-                                <p>Scanner le<br /><br />QR Code du Site <BsArrowRight className='card-arrow' /></p>
-                                <img src={qr} alt="qr code" />
-                            </div>
-                        </div>
-                    )
-                })
-            }
+            {currentCards.map(({ id, image, title, demo, git, qr }) => (
+                <div className="card" key={id}>
+                    <div className="card-image">
+                        <img src={image} alt="ilustartion site" />
+                        <a href={demo} target="_blank" rel="noreferrer" className='card-link'>{title}</a>
+                        <a href={git} target="_blank" rel="noreferrer"><BsGithub className='link' /></a>
+                    </div>
+                    <div className="card-qr">
+                        <p>Scanner le<br /><br />QR Code du Site <BsArrowRight className='card-arrow' /></p>
+                        <img src={qr} alt="qr code" />
+                    </div>
+                </div>
+            ))}
         </div>
-    )
+        
+                {/* Afficher les boutons de pagination */ }
+        <div className="pagination">
+            {data.length > cardsPerPage && (
+                Array.from({ length: Math.ceil(data.length / cardsPerPage) }, (_, index) => (
+                    <button
+                        key={index + 1}
+                        className={currentPage === index + 1 ? 'active' : ''}
+                        onClick={() => paginate(index + 1)}
+                    >
+                        {index + 1}
+                    </button>
+                ))
+            )}
+        </div>
+    </>  
+    );
 }
 
-export default Sites
+export default Sites;
