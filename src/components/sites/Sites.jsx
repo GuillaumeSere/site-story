5ec3d3470dd6e06edd4f78557be398ad3959c237
Guillaume Sere <guillaumesere@hotmail.fr>
2023-06-06 15:11:38 +0200

    modif header

--- src/components/sites/Sites.jsx
+++ src/components/sites/Sites.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './sites.css';
 import { BsArrowRight, BsGithub } from 'react-icons/bs';
+import { MdOutlineHome } from 'react-icons/md';
 import data from '../data/Data';
 
 const Sites = () => {
@@ -17,37 +18,42 @@ const Sites = () => {
 
     return (
         <>
-        <div className='container'>
-            {currentCards.map(({ id, image, title, demo, git, qr }) => (
-                <div className="card" key={id}>
-                    <div className="card-image">
-                        <img src={image} alt="ilustartion site" />
-                        <a href={demo} target="_blank" rel="noreferrer" className='card-link'>{title}</a>
-                        <a href={git} target="_blank" rel="noreferrer"><BsGithub className='link' /></a>
+            <button
+                className='btn-home'
+                onClick={() => paginate(1)}>
+                <MdOutlineHome className='icon' />
+            </button>
+            <div className='container'>
+                {currentCards.map(({ id, image, title, demo, git, qr }) => (
+                    <div className="card" key={id}>
+                        <div className="card-image">
+                            <img src={image} alt="ilustartion site" />
+                            <a href={demo} target="_blank" rel="noreferrer" className='card-link'>{title}</a>
+                            <a href={git} target="_blank" rel="noreferrer"><BsGithub className='link' /></a>
+                        </div>
+                        <div className="card-qr">
+                            <p>Scanner le<br /><br />QR Code du Site <BsArrowRight className='card-arrow' /></p>
+                            <img src={qr} alt="qr code" />
+                        </div>
                     </div>
-                    <div className="card-qr">
-                        <p>Scanner le<br /><br />QR Code du Site <BsArrowRight className='card-arrow' /></p>
-                        <img src={qr} alt="qr code" />
-                    </div>
-                </div>
-            ))}
-        </div>
-        
-                {/* Afficher les boutons de pagination */ }
-        <div className="pagination">
-            {data.length > cardsPerPage && (
-                Array.from({ length: Math.ceil(data.length / cardsPerPage) }, (_, index) => (
-                    <button
-                        key={index + 1}
-                        className={currentPage === index + 1 ? 'active' : ''}
-                        onClick={() => paginate(index + 1)}
-                    >
-                        {index + 1}
-                    </button>
-                ))
-            )}
-        </div>
-    </>  
+                ))}
+            </div>
+
+            {/* Afficher les boutons de pagination */}
+            <div className="pagination">
+                {data.length > cardsPerPage && (
+                    Array.from({ length: Math.ceil(data.length / cardsPerPage) }, (_, index) => (
+                        <button
+                            key={index + 1}
+                            className={currentPage === index + 1 ? 'active' : ''}
+                            onClick={() => paginate(index + 1)}
+                        >
+                            {index + 1}
+                        </button>
+                    ))
+                )}
+            </div>
+        </>
     );
 }
 
